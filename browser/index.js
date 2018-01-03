6614b58107f6bb5117572e897e9ac30611d38143
Martin Høgh <mh@mapcentia.com>
2018-01-03 09:18:52 +0100

    About box

--- browser/index.js
+++ browser/index.js
@@ -114,7 +114,7 @@ var sourceShare =
     '<div id="share-buttons" style="text-align: center" class="bs-component btn-group-sm">' +
     '<a href="javascript:void(0)" class="btn btn-default btn-fab btn-share" data-some-site="facebook" data-poi-id="{{id}}"><i class="material-icons fa fa-facebook"></i></a>' +
     '<a href="javascript:void(0)" class="btn btn-default btn-fab btn-share" data-some-site="twitter" data-poi-id="{{id}}"><i class="material-icons fa fa-twitter"></i></a>' +
-    '<a href="javascript:void(0)" class="btn btn-default btn-fab btn-share" data-todo-id="{{id}}"><i class="material-icons">directions</i></a>' +
+    '<a href="javascript:void(0)" class="btn btn-info btn-fab btn-share" data-todo-id="{{id}}"><i class="material-icons">directions</i></a>' +
     '</div>' +
 
     '<script>' +
